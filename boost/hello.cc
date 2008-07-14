6a50263e454de22e86a6c5f082d24f941432f99b
Robert Stine <bob@hnt-up-dhcp-260.wharton.upenn.edu>
2008-07-14 11:34:19 -0400

    from school; make script damaged?

--- boost/hello.cc
+++ boost/hello.cc
@@ -1,13 +1,16 @@
-#include <boost/lambda/lambda.hpp>
+// #include <boost/lambda/lambda.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 
 int main()
 {
-  using namespace boost::lambda;
+  // using namespace boost::lambda;
   typedef std::istream_iterator<int> in;
 
-  std::for_each(
+  /*
+    std::for_each(
 		in(std::cin), in(), std::cout << (_1 * 3) << " " );
+  */
+  std::cout << "HJERE\n";
 }
