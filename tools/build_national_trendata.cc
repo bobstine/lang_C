80509275414cbc988caad597dacf20883d712eb3
Bob Stine <stine@wharton.upenn.edu>
2010-02-01 08:13:27 -0500

    done for time being with national, now moving on to state

--- tools/build_national_trendata.cc
+++ tools/build_national_trendata.cc
@@ -25,19 +25,23 @@
 #include "read_utils.h"
 
 int
-row_number(double dyear)               // format is 1992.1, 1992.2, 1992.3, 1992.4, 1993.1, ...
+row_number(std::string date)               // format is 1992.1, 1992.2, 1992.3, 1992.4, 1993.1, ...
 {
-  int year (dyear);
-  int quarter (10.0*(dyear-(double)year));
-  return 4*(year-1992) + (quarter-1);
+  date[4] = ' ';
+  int iyear   ;
+  int quarter ;
+  std::istringstream input(date);
+  input >> iyear >> quarter;
+  return 4*(iyear-1992)+quarter-1;
 }
 
 
 int
 main()
 {
-  const int num_quarters (71);                              //   1992:1 .. 2009:3
-
+  const int    num_quarters  (71);                              //   1992:1 .. 2009:3
+  const double missing_value (-7.777777);
+  
   std::vector<std::string> dataFileNames;
   dataFileNames.push_back("raw_data_1.csv");
   //  dataFileNames.push_back("raw_data_2.csv");
@@ -57,63 +61,72 @@ main()
     while(!dataFile.eof())
     { // keep track of lines read
       ++lineCount;
-      std::cout << lineCount << std::endl;
-      if (0 == lineCount % 100) std::cout << "Line Count @ " << lineCount << std::endl;
+      if (0 == lineCount % 1000) std::cout << "Line Count @ " << lineCount << std::endl;
       // read whole line from file
       std::string line;                
       getline(dataFile, line);
-      std::cout << "Read line " << line << std::endl;
+      // std::cout << "Read line " << line << std::endl;
+      if(line.empty()) break;
       // process quoted tokens
       std::vector<std::string> strs (3);
       int s (0);
       std::string::const_iterator i (line.begin());
-      assert(*i == '"');
+      if (*i != '"')
+      { std::cerr << "Leading character is not a double quote; ending.\n";
+	break;
+      }
       ++i;                                                      // skip initial "
       while(s<3)
-      { std::cout << "top with *i = " << *i << std::endl;
+      { // std::cout << "top with *i = " << *i << std::endl;
 	if (*i == '"')
 	{ ++s;
-	  std::cout << "        delimiter with s=" << s << std::endl;
 	  if (s < 3)
 	  { ++i;
-	    std::cout << "Asserting comma\n";
 	    assert (*i==',');
-	    i = i+2;                                    // skip ,"
+	    i = i+2;                                            // skip ,"
 	  }
 	}
 	else
-	{ std::cout << "Pushing back " << *i << std::endl;
+	{ //std::cout << "Pushing back " << *i << std::endl;
 	  strs[s].push_back(*i);
-	  std::cout << "Incrementing\n";
 	  ++i;
 	}
       }
       // convert to values
-      std::cout << "Converting: " << strs[0] << "  " << strs[1] << "  " << strs[2] << std::endl;
-      double quarter (read_utils::lexical_cast<double>(strs[0].c_str()));
-      int row = row_number(quarter);
-      assert (row <= numQuarters);
+      // std::cout << "Converting: " << strs[0] << "  " << strs[1] << "  " << strs[2] << std::endl;
+      int row = row_number(strs[0]);
+      assert (row <= num_quarters);
+      assert (0 <= row);
       double value   (read_utils::lexical_cast<double>(strs[2].c_str()));
-      std::cout << "Conversion done...inserting into row " << row << "\n";
       // store in map 
-      if (varNames.find(strs[1]) == varNames.end())       // add new column;
-      { std::cout << "New variable is " << strs[1] << std::endl;
-	data[strs[1]] = std::vector<double>(num_quarters);
+      if (varNames.find(strs[1]) == varNames.end())       // add vector initialized with missing value
+      { std::cout << "Found new variable:  " << strs[1] << std::endl;
+	varNames.insert(strs[1]);
+	data[strs[1]] = std::vector<double>(num_quarters, missing_value);
       }
-      std::cout << "Doing insertion\n";
+      // std::cout << "Insert " << value << " -> " << strs[1] << "[" << row << "]\n";
       data[strs[1]][row] = value;
     }
-    std::cout << "Completed reading file: " << *f << std::endl;
+    std::cout << "Completed reading file: " << *f
+	      << ".  Read " << lineCount << " input lines with " << varNames.size() << " variables.\n";
   }
   std::cout << "Preparing to write data to file national.td\n";
   // write data out, starting with var names
   std::ofstream output ("national.td");
-  for(std::set<std::string>::const_iterator i = varNames.begin(); i != varNames.end(); ++i)
-    output << *i << "\t";
-  for(std::set<std::string>::const_iterator i = varNames.begin(); i != varNames.end(); ++i)
-  { output << std::endl;
-    for(int q=0; q < num_quarters; ++q)
-      output << data[*i][q] << "\t";
+  std::set<std::string>::const_iterator name (varNames.begin());
+  output << *name;
+  for(++name; name != varNames.end(); ++name)
+    output << "\t" << *name;
+  for(int q=0; q < num_quarters; ++q)
+  { output << "\n";
+    name = varNames.begin();
+    output << data[*name][q];
+    for(++name; name != varNames.end(); ++name)
+    { if (data[*name][q] == missing_value)
+	output << "\t NA";
+      else
+	output << "\t " << data[*name][q];
+    }
   }
 }
 
