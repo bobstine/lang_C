a2a855e63ae73b977e5718c3981bd543d0f51765
Bob at Penn <stine@wharton.upenn.edu>
2010-08-02 17:30:39 -0400

    playing with openMP

--- TestPrograms/hello.cc
+++ TestPrograms/hello.cc
@@ -1,6 +1,10 @@
+#include <omp.h>
 #include <iostream>
 
 main()
 {
-  std::cout << "Hdellw" << std::endl;
+  std::cout << "Hello" << std::endl;
+#pragma omp parallel
+  std::cout << "Hello from thread " << omp_get_thread_num()
+	    << " out of " << omp_get_num_threads() << " threads.\n";
 }
