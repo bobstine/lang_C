35ff0ee72f2a7ff5140a302d48c79ff8101a7ed2
Bob Stine <stine@wharton.upenn.edu>
2012-01-30 07:04:58 -0500

    push-all update

--- tools/bellman.cc
+++ tools/bellman.cc
@@ -51,7 +51,7 @@ private:
   const double mGamma;
   const double mOmega;
   
-  int    mK;
+  double mBetaK;
   double mV0, mVkp1;
   
 public:
@@ -59,36 +59,44 @@ public:
     : mGamma(gamma), mOmega(omega) {}
 
   void set_k (int k, double v0, double vkp1)
-    { mK = k; mV0 = v0; mVkp1 = vkp1;  }
+    { mBetaK = beta(k,mOmega); mV0 = v0; mVkp1 = vkp1;  }
 
   double operator()(double mu) const
     {
-      double b = beta(mK, mOmega);
       if(mu < 0.00001)
       { // std::cout << "For mu = 0 reject prob r = b = " << b << " and V0 = " << mV0 << " and V_k+1 = " << mVkp1 << std::endl;
-	return mGamma * b * (1.0-mOmega) + b*mV0 + (1-b)*mVkp1;
+	return mGamma * mBetaK * (1.0-mOmega) + mBetaK*mV0 + (1-mBetaK)*mVkp1;
       }
       else
       {
-	double rb = reject(mu,b);
+	double rb = reject(mu,mBetaK);
 	double a = alpha(mu);
 	double ra = reject(mu,a);
-	double gain = (mOmega * ra - a) - mGamma * (mOmega * rb - b) + rb * mV0 + (1-rb) * mVkp1;
+	double gain = (mOmega * ra - a) - mGamma * (mOmega * rb - mBetaK) + rb * mV0 + (1-rb) * mVkp1;
 	return gain;
       }
     }
   
-  DoublePair change_in_value(double mu, int k) const    // expert and bidder
+  double value_to_oracle(double mu, double o0, double okp1) const    // expert 
     {
-      double b = beta(k,mOmega);
+      double value, rb;
       if (mu < 0.00001)
-	return std::make_pair(0, b*(mOmega-1));
-      else
-      { double rb = reject(mu,b);
-	double a = alpha(mu);
-	double ra = reject(mu,a);
-	return std::make_pair(mOmega * ra - a, mOmega * rb - b);
+      { value = 0.0;
+	rb = mBetaK;
       }
+      else
+      { double a = alpha(mu);
+	double ra = reject(mu,a);
+	value = mOmega * ra - a ;
+	rb = reject(mu, mBetaK);
+      }
+      return value + rb * o0 + (1-rb) * okp1;
+    }
+  
+  double value_to_bidder (double mu, double b0, double bkp1) const    // universal bidder
+    {
+      double rb = (mu < 0.00001) ? mBetaK : reject(mu,mBetaK);
+      return mOmega * rb - mBetaK + rb * b0 + (1-rb) * bkp1;
     }    
   
 private: 
@@ -103,23 +111,28 @@ private:
 
 };
 
+
 void
-solve_bellman_equation (double gamma, double omega, Eigen::MatrixXf &gain, Eigen::MatrixXf &mean)
+solve_bellman_equation (double gamma, double omega, Eigen::MatrixXf &gain, Eigen::MatrixXf &mean, Eigen::MatrixXf &oracle, Eigen::MatrixXf &bidder)
 {
-  Line_Search::GoldenSection search(.0001, std::make_pair(1.5,4.0), 100);
+  const int maxIterations (100);
+  const double tolerance  (0.0001);
+  Line_Search::GoldenSection search(tolerance, std::make_pair(1.5,4.0), maxIterations);
   ExpertCompetitiveGain compRatio (gamma, omega);
     
-  // initial boundary condition
+  // initial boundary condition goes into extra row
   double v0   = omega - gamma * omega;
-  //  std::cout << "BELL: Initial value is " << v0 << std::endl;  
   int maxRow = gain.rows();
   for (int j=0; j<maxRow; ++j)
-  { gain(maxRow-1,j) = v0;
-    mean(maxRow-1,j) = 0.0;
+  { gain(maxRow-1,j)  = v0;
+    oracle(maxRow-1,j)=omega;
+    bidder(maxRow-1,j)=omega;
   }
   // fill rows in triangular array
   for (int row = maxRow-2; row > -1; --row)
   { v0 = gain(row+1,0);
+    double b0 = bidder(row+1,0);
+    double o0 = oracle(row+1,0);
     for (int col=0; col<=row; ++col)
     { std::pair<double,double> maxPair;
       compRatio.set_k(col, v0, gain(row+1,col+1));
@@ -127,8 +140,10 @@ solve_bellman_equation (double gamma, double omega, Eigen::MatrixXf &gain, Eigen
       maxPair = search.find_maximum(compRatio);
       if (maxPair.second < atZero)
 	maxPair = std::make_pair(0.0,atZero);
-      mean(row,col) = maxPair.first;
-      gain(row,col) = maxPair.second;
+      mean  (row,col) = maxPair.first;
+      gain  (row,col) = maxPair.second;
+      oracle(row,col) = compRatio.value_to_oracle(maxPair.first, o0, oracle(row+1,col+1));
+      bidder(row,col) = compRatio.value_to_bidder(maxPair.first, b0, bidder(row+1,col+1));
     }
   }
 }
@@ -149,38 +164,58 @@ write_matrix_to_file (std::string fileName, Eigen::MatrixXf const& x)
   return 0;
 }
 
+
+int
+write_vector_to_file (std::string fileName, Eigen::VectorXf const& x)
+{
+  std::ofstream output (fileName.c_str());
+  if (! output)
+  { std::cerr << "ERROR: Cannot open output text file for writing " << fileName << std::endl;
+    return 1;
+  }
+  for (int i=0; i<x.size(); ++i)
+      output << x(i) << " ";
+  output << std::endl;
+  return 0;
+}
+
+
 int  main(int argc, char** argv)
 {
   const double omega  = 0.05;
-  
+
+  // default arguments
   double     gamma  = 2.5;
   int       maxRow  = 100;
   bool   writeTable = false;    // if false, only return final value
 
-  std::cout << "PARSING ARGUMENTS\n";
   parse_arguments(argc, argv, gamma, maxRow, writeTable);
-  std::cout << "DONE ARGUMENTS\n";
+  std::cout << gamma << " " << maxRow << " " << writeTable << " ";
+  
+  Eigen::MatrixXf gain  (maxRow+1, maxRow+1);   // extra row for boundary condition
+  Eigen::MatrixXf oracle(maxRow+1, maxRow+1);
+  Eigen::MatrixXf bidder(maxRow+1, maxRow+1);
+  Eigen::MatrixXf mean  (maxRow,   maxRow);
 
-  std::cout << "BELL: options   g=" << gamma << "   n=" << maxRow << "  writeTable=" << writeTable << std::endl;
-  
-  Eigen::MatrixXf gain(maxRow+1,maxRow+1);
-  Eigen::MatrixXf mean(maxRow+1,maxRow+1);
-  
-  solve_bellman_equation (gamma, omega, gain, mean);
+  solve_bellman_equation (gamma, omega, gain, mean, oracle, bidder);
   
   // write solution (without boundary row) to file
   if(writeTable)
   { int gammaInt (trunc(10 *gamma));
     std::ostringstream ss;
-    ss << "bellman.g" << gammaInt << ".n" << maxRow;
-    std::string fileName  (ss.str() + ".mu");
-    write_matrix_to_file(fileName, mean.topLeftCorner(mean.rows()-1, mean.rows()-1));  // omit boundary row
-    fileName  = ss.str() + ".gain";
-    write_matrix_to_file(fileName, mean.topLeftCorner(mean.rows()-1, gain.rows()-1));
+    ss << "bellman.g" << gammaInt << ".n" << maxRow << ".";
+    std::string fileName  (ss.str() + "mu");
+    write_matrix_to_file(fileName, mean.topLeftCorner(mean.rows(), mean.rows()));
+    fileName  = ss.str() + "gain";
+    write_matrix_to_file(fileName, gain.topLeftCorner(gain.rows()-1, gain.rows()-1));  // omit boundary row
+    fileName = ss.str() + "oracle";
+    write_matrix_to_file(fileName, oracle.topLeftCorner(oracle.rows()-1,oracle.rows()-1));
+    fileName = ss.str() + "bidder";
+    write_matrix_to_file(fileName, bidder.topLeftCorner(bidder.rows()-1, bidder.rows()-1));
   }
   
-  // write the final value to std io
-  std::cout << gain(0,0) << std::endl;
+  // write the final values to std io
+  std::cout << gain(0,0) << " " << oracle(0,0) << " " << bidder(0,0) << std::endl;
   
   return 0;
 }
