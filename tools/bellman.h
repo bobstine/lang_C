0fdee5e09ce64026871df7fa2061c81cd56c6957
Bob at Penn <stine@wharton.upenn.edu>
2012-02-02 17:35:29 -0500

    rearrange bellman code for constrained expert

---
+++ tools/bellman.h
@@ -0,0 +1,110 @@
+#include <utility>         // pair
+#include <functional>
+
+/***********************************************************************************
+
+  Bellman backward recursion for comptitive alpha-investing using
+  several possible probability distributions, with spending percentage.
+
+***********************************************************************************/
+
+typedef double (*ProbDist)(int,int);
+
+void
+solve_bellman_equation (double gamma, double omega, int nRounds, double spendPct, ProbDist f, bool writeDetails);
+
+void
+solve_constrained_bellman_equation (double gamma, double omega, int nRounds, double spendPct, ProbDist oracleProb, ProbDist bidderProb);
+
+
+
+
+/**********************************************************************************
+
+   Probability distributions that control spending
+
+     All take 2 integer arguments (k since reject, j remaining)
+     but some (like universal) don't use the second argument
+
+ **********************************************************************************/
+
+double universal (int k, int);
+
+double geometric (int k, int);
+
+double equal (int k, int left);         // equal spread over possible tests since reject
+
+
+
+//  This guy does the optimization to find the best mu at given state
+//  with no constraint, a real oracle.
+//
+//  This operator has one state item, k, the number of rounds since the
+//  last time the bidder rejected.  The bidder is deterministic, controlled
+//  by the input probablity distribution (defined above).
+
+class ExpertCompetitiveGain: public std::unary_function<double,double>
+{
+ private:
+  const double mGamma;
+  const double mOmega;
+  const ProbDist mProb;
+  const double mSpendPct;
+  double mBetaK;
+  double mV0, mVkp1;
+  
+ public:
+
+ ExpertCompetitiveGain(double gamma, double omega, ProbDist f, double spendPct)
+   : mGamma(gamma), mOmega(omega), mProb(f), mSpendPct(spendPct), mBetaK(0.0) {}
+  
+  double beta_k (void) const { return mBetaK; }
+  
+  void set_k (int k, int left, double v0, double vkp1)
+  { mBetaK = mOmega * mSpendPct * mProb(k,left);
+    mV0 = v0; mVkp1 = vkp1;  }
+  
+  double operator()(double mu) const;
+  double value_to_oracle (double mu, double o0, double okp1) const;
+  double value_to_bidder (double mu, double b0, double bkp1) const;
+  
+ private:
+  double alpha (double mu) const;
+}; 
+
+
+
+//  This expert is control by a geometric spending policy since its last
+//  rejection, so its state depends on 2 things: the number of tests since
+//  its last rejection and the number since the  bidder's rejection.
+
+class ConstrainedExpertCompetitiveGain: public std::unary_function<double,double>
+{
+ private:
+  const double mGamma;
+  const double mOmega;
+  const ProbDist mExpertProb, mBidderProb;
+  const double mSpendPct;
+  double mAlpha, mBeta;
+  double mV00, mVi0, mVij, mV0j;
+  
+ public:
+
+ ConstrainedExpertCompetitiveGain(double gamma, double omega, double spendPct, ProbDist expertP, ProbDist bidderP)
+   : mGamma(gamma), mOmega(omega), mExpertProb(expertP), mBidderProb(bidderP), mSpendPct(spendPct), mAlpha(0.0), mBeta(0.0) {}
+
+  double alpha (void) const { return mAlpha; }
+  double beta  (void) const { return mBeta; }
+  
+  void set_delay (int i, int j, int t, int nRounds, double v00, double vi0, double v0j, double vij)
+  { mAlpha = mOmega * mSpendPct * mExpertProb(i,nRounds-t);
+    mBeta  = mOmega * mSpendPct * mBidderProb(j,nRounds-t);
+    mV00 = v00; mVi0 = vi0; mV0j = v0j; mVij = vij;
+  }
+  
+  double operator()(double mu) const;
+  double value_to_oracle (double mu, double o0, double okp1) const;
+  double value_to_bidder (double mu, double b0, double bkp1) const;  
+
+};  // class
+
---
+++ tools/bellman_solver.cc
@@ -0,0 +1,96 @@
+#include "bellman.h"
+
+#include <math.h>
+
+#include <iostream>
+#include <getopt.h>
+#include "read_utils.h"     
+
+
+
+void
+parse_arguments(int argc, char** argv,     double &gamma, int &nRounds, char &probChar, double &spendPct, bool &writeTable);
+
+int  main(int argc, char** argv)
+{
+  const double omega  = 0.05;
+
+  // default arguments
+  double     gamma  = 2.5;
+  int       maxRow  = 100;
+  double  spendPct  = 0.5;
+  char    probChar  = 'u';
+  bool   writeTable = false;    // if false, only return final value
+  
+  parse_arguments(argc, argv, gamma, maxRow, probChar, spendPct, writeTable);
+  
+  // select function for spending down probability
+  ProbDist p;
+  switch (probChar)
+  {
+  case 'u': { p = universal; break; }
+  case 'g': { p = geometric; break; }
+  case 'e': { p = equal;     break; }
+  default: { std::cerr << "ERROR: Unrecognized probablity distribution " << probChar << " chosen.\n"; return -1; }
+  }
+  
+  // output parameters and let solver tack on results: e-gamma b, e, b
+  std::cout << gamma << " " << maxRow << " " << writeTable << " " << probChar << " " << spendPct << " ";
+  solve_bellman_equation (gamma, omega, maxRow, spendPct, p, true);
+  
+  return 0;
+}
+
+
+
+void
+parse_arguments(int argc, char** argv,		double &gamma, int &nRounds, char &probChar, double &spendPct, bool &writeTable)
+{
+  static struct option long_options[] = {
+    {"gamma",   required_argument, 0, 'g'},
+    {"rounds",  required_argument, 0, 'n'},
+    {"prob",    required_argument, 0, 'p'},
+    {"spend",   required_argument, 0, 's'},
+    {"write",         no_argument, 0, 'w'},
+    {0, 0, 0, 0}                             // terminator 
+  };
+  int key;
+  int option_index = 0;
+  while (-1 !=(key = getopt_long (argc, argv, "g:n:p:s:w", long_options, &option_index))) // colon means has argument
+  {
+    // std::cout << "Option key " << char(key) << " for option " << long_options[option_index].name << ", option_index=" << option_index << std::endl;
+    switch (key)
+    {
+    case 'g' : 
+      {
+	gamma = read_utils::lexical_cast<double>(optarg);
+	break;
+      }
+    case 'n' :
+      {
+	nRounds = read_utils::lexical_cast<int>(optarg);
+	break;
+      }
+    case 'p' :
+      {
+	probChar = read_utils::lexical_cast<char>(optarg);
+	break;
+      }
+    case 's' :
+      {
+	spendPct = read_utils::lexical_cast<double>(optarg);
+	break;
+      }
+    case 'w' : 
+      {
+	writeTable=true ;
+	break;
+      }
+    default:
+      {
+	std::cout << "PARSE: Option not recognized; returning.\n";
+      }
+    } // switch
+  } // while
+}
+
