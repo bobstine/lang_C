ee670597cc7089bff54721773516191cb7a71590
Bob Stine <stine@wharton.upenn.edu>
2014-03-04 22:01:11 -0500

    mod to timing code to remove string stream

--- utils/timing.cc
+++ utils/timing.cc
@@ -2,7 +2,6 @@
 
 #include <time.h>
 #include <iostream>
-#include <sstream>
 
 
 void
@@ -12,9 +11,7 @@ print_with_time_stamp(std::string str, std::ostream &os)
   struct tm * timeinfo;
   time ( &rawtime );
   timeinfo = localtime ( &rawtime );
-  std::ostringstream ss;
-  ss << "TIME: " << str << " @ " << std::string(  asctime (timeinfo) ) << std::endl;
-  os << ss.str();
+  os << "TIME: " << str << " @ " << std::string(  asctime (timeinfo) ) << std::endl;
 }
 
 
@@ -22,7 +19,5 @@ void
 print_with_elapsed_seconds(std::string const& s, clock_t const& start, clock_t const& stop, std::ostream &os)
 {
   clock_t diff (stop-start);
-  std::ostringstream oss;
-  oss <<  "TIME: " << s << " [" << ((float)diff)/CLOCKS_PER_SEC << " sec]" << std::endl;
-  os << oss.str();
+  os <<  "TIME: " << s << " [" << ((float)diff)/CLOCKS_PER_SEC << " sec]" << std::endl;
 }
