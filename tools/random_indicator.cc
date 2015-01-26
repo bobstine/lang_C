08d7abcf272f8de1b9dce587fed665e99f27988e
Bob Stine <stine@wharton.upenn.edu>
2015-01-25 22:24:09 -0500

    random_indicator allows proportion or count for input

--- tools/random_indicator.cc
+++ tools/random_indicator.cc
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <stdlib.h>
 #include <getopt.h>
 #include <assert.h>
@@ -21,7 +22,7 @@ parse_arguments(int argc, char** argv,
 		bool   &header,           // add lines with name, attributes
 		int    &prefix,           // number of prefix 1 blocks
 		int    &n,                // number random items
-		int    &c,                // number of 1s
+		double &pc,               // proportion to use (if <= 1, or count to use if > 1)
 		int    &blockSize,        // blocking factor, such as number of counties
 		int    &seed);              
 
@@ -31,31 +32,31 @@ int main(int argc, char ** argv)
   bool headerLines = false;
   int prefixCases  = 0;
   int n            = 0;
-  int c            = 0;
+  double choose    = 0;
   int blockSize    = 1;
   int seed         = 2741;
 
-  parse_arguments(argc, argv, headerLines,  prefixCases, n,c, blockSize,seed);
-  std::clog << "random_indicator --prefix=" << prefixCases << " -n" << n << " -c" << c
+  parse_arguments(argc, argv, headerLines,  prefixCases, n, choose, blockSize,seed);
+  std::clog << "random_indicator --prefix=" << prefixCases << " -n" << n << " -c" << choose
 	    << " --blocksize=" << blockSize << " --seed=" << seed;
   if (headerLines) std::clog << " --header";
   std::clog << std::endl;
-  assert(c < n);
-  
 
   if (headerLines) std::cout << "CV\ncv indicator\n";
   srand48(seed);
   for (int i=0; i<prefixCases; ++i)
     for (int j=0; j<blockSize; ++j)
       std::cout << "1\t";
-  double dc = (double) c;
+  if (choose <= 1.0)
+    choose = (double) floor(choose * (double)n);
+  assert(choose < n);
   int value;
   ++n;
   while (--n)
-  { double p = dc/(double)n;       // std::cout << "\n c=" << dc << "/n=" << n << "  p=" << p << " ";
+  { double p = choose/(double)n;       // std::cout << "\n c=" << dc << "/n=" << n << "  p=" << p << " ";
     if(drand48() < p)
     { value = 1;
-      dc -= 1.0;
+      choose -= 1.0;
     }
     else value = 0;
     for(int i=0; i<blockSize; ++i)
@@ -72,7 +73,7 @@ parse_arguments(int argc, char** argv,
 		bool   &header,
 		int    &prefix,              // number of prefix 1 blocks
 		int    &n,                   // number random items
-		int    &c,                   // number of 1s
+		double &c,                   // number of 1s or proportion 1
 		int    &blockSize,           // blocking factor, such as number of counties
 		int    &seed)                // for rand
 {
@@ -98,7 +99,7 @@ parse_arguments(int argc, char** argv,
 	  {
 	  case 'n' : { n         = read_utils::lexical_cast<int>(optarg);    break; }
 	  case 'p' : { prefix    = read_utils::lexical_cast<int>(optarg);    break; }
-	  case 'c' : { c         = read_utils::lexical_cast<int>(optarg);    break; }
+	  case 'c' : { c         = read_utils::lexical_cast<double>(optarg); break; }
 	  case 'b' : { blockSize = read_utils::lexical_cast<int>(optarg);    break; }
 	  case 's' : { seed      = read_utils::lexical_cast<int>(optarg);    break; }
 	  case 'v' : { header    = true;                                     break; }      
